a98066f5e1c3e2133f30a813ebddb2a75f9c4641
totalgloria <litonghit@gmail.com>
2017-04-26 20:37:39 -0500

    Add files via upload

---
+++ assignment2_Tong Li.R
@@ -0,0 +1,32 @@
+## Assignment 2: Caching the Inverse of a Matrix
+## Author: Tong Li
+
+## This function creates a special matrix object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <-function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <-function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
